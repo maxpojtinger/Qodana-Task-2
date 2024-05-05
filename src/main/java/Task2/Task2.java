d01e032f11c969206bf642a5443ea28aac8ebc3e
Max <ge89suw@mytum.de>
2024-05-05 20:27:38 +0200

    second push

--- src/main/java/Task2/Task2.java
+++ src/main/java/Task2/Task2.java
@@ -1,7 +1,6 @@
 package Task2;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
